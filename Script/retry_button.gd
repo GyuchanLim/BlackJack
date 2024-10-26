b2777a889abcadd5ddac1913aaa62896dd6a664a
Gyuchan Lim <86847942+GyuchanLim@users.noreply.github.com>
2024-10-27 10:43:26 +1300

    Update retry_button.gd
    remove bad code

--- Script/retry_button.gd
+++ Script/retry_button.gd
@@ -1,7 +1,6 @@
 extends Button
 
 @onready var play = get_node("../")
-@onready var game_control = get_node("../gameControl")
 @onready var play_button = get_node("../playButton")
 
 func _on_retry_button_pressed() -> void:
